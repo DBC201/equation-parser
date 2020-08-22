1258144def664503f9ae85a9172e803302fee529
DBC201 <denizbcakiroglu@gmail.com>
2020-08-22 17:12:46 -0400

    setup.py update

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     author_email="denizbcakiroglu@gmail.com",
     packages=["equation"],
     url="https://github.com/DBC201/equation-parser",
-    license='LICENSE.txt',
     description="An equation package currently containing a parser",
     long_description=open('Readme.txt').read(),
     python_requires='>=3.6',
